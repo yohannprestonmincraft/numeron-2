229cd0e3ffc45ca23677fcc46bce245913888d09
yohannprestonmincraft <83777115+yohannprestonmincraft@users.noreply.github.com>
2021-07-08 18:42:28 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# numeron-2
+# numeron2_boilerplatee
---
+++ app.js
@@ -0,0 +1,8 @@
+//link the play button
+
+const playButton = document.getElementById("play-button")
+
+playButton.onclick=()=>{
+    location.href = "./game.html"
+
+}
